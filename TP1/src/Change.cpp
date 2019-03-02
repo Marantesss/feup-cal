e1d5370cd24ead9e65114eee7fe4dc9b392b415a
Marantesss <up201706917@fe.up.pt>
2019-03-02 15:09:53 +0000

    Finally did exercise 2 correctly

--- TP1/src/Change.cpp
+++ TP1/src/Change.cpp
@@ -3,11 +3,54 @@
  */
 
 #include "Change.h"
+#include <algorithm>
 
 // coinValues[] = {1, 2, 5}
 // exite um stock ilimitado de moedas de cada valor
 string calcChange(int m, int numCoins, int *coinValues)
 {
+	int minCoin[m + 1];
+	int lastCoin[m + 1];
+	for (int i = 0; i < m + 1; i++) {
+		minCoin[i] = 100;
+		lastCoin[i] = 0;
+	}
+	minCoin[0] = 0;
+
+	for (int i = 0; i < numCoins; i++) {
+		int possibleLastCoin = coinValues[i];
+		for (int k = 1; k <= m; k++) {
+			int remainingChange = k - possibleLastCoin;
+			if (remainingChange >= 0) {
+				int possibleMinCoin = minCoin[remainingChange] + 1;
+				if (possibleMinCoin < minCoin[k]) {
+					minCoin[k] = possibleMinCoin;
+					lastCoin[k] = possibleLastCoin;
+				}
+			}
+		}
+	}
+
+	// printing the result
+	for (int k = 0; k <= m; k++)
+		cout << k << "    \t";
+	cout << endl;
+	for (int k = 0; k <= m; k++)
+		cout << minCoin[k] << " - " << lastCoin[k] << "\t";
+
+	// getting the result
+	int remainingChange = m;
+	string answer = "";
+	while (remainingChange != 0) {
+		answer += to_string(lastCoin[remainingChange]) + ";";
+		remainingChange -= lastCoin[remainingChange];
+	}
+	cout << answer << endl;
+	return answer;
+
+	/*
+	 * Greedy Algorithm
+	 *
 	int missingChange;
 	string change[100] = {""};
 	if (m - coinValues[0] < 0 && m != 0)
@@ -29,6 +72,7 @@ string calcChange(int m, int numCoins, int *coinValues)
 		cout << change[i] << endl;
 	}
     return change[m];
+    */
 }
 
 
